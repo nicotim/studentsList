8322ddf91a4886c5eb64aaa05fdf33e6d546fd3c
nicotim <nik21timo@gmail.com>
2023-11-29 17:11:01 -0300

    Cerrar Sesion button

--- src/app/dashboard/pages/inscriptions/inscriptions.module.ts
+++ src/app/dashboard/pages/inscriptions/inscriptions.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InscriptionsComponent } from './inscriptions.component';
 import { InscriptionsRoutingModule } from './inscriptions-routing.module';
+import { EffectsModule } from '@ngrx/effects';
+import { InscriptionEffects } from './store/inscription.effects';
 
 @NgModule({
   declarations: [InscriptionsComponent],
-  imports: [CommonModule, InscriptionsRoutingModule],
+  imports: [CommonModule, InscriptionsRoutingModule, EffectsModule.forFeature([InscriptionEffects])],
 })
 export class InscriptionsModule {}
